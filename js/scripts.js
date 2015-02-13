15638dec278890ed5741e4e641552caf11a5e32f
Shimran <student@epicodus.com>
2015-02-13 11:34:08 -0800

    Add spec, add logic, and pass spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-var triangle = (function(sideOne, sideTwo, sideThree) {
+var Triangle = (function(sides) {
 
+  var newSides = sides.split(",");
+  var sideOne = parseFloat(newSides[0]);
+  var sideTwo = parseFloat(newSides[1]);
+  var sideThree = parseFloat(newSides[2]);
 
-return "Side one equals" + sideOne;
+    if (sideOne + sideTwo < sideThree || sideOne + sideThree < sideTwo || sideTwo + sideThree < sideOne){
+      return "These are not valid triangle dimensions"
+    }else if (sideOne==sideTwo && sideOne!==sideThree || sideOne===sideThree && sideOne!==sideTwo || sideTwo=== sideThree && sideTwo!==sideOne){
+      return "You have an Isosceles Triangle"
+    }else if (sideOne === sideTwo && sideTwo === sideThree){
+      return "You have an Equilateral Triangle"
+    }else{
+      return "You have a Scalene Triangle"
+    }
+
+});
 
 
 // $(document).ready(function() {
@@ -11,4 +25,4 @@ return "Side one equals" + sideOne;
 //     $("#result").show();
 //     event.preventDefault();
 //   });
-});
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,26 @@
-describe('triangle', function(){
-  it('will output the length of side one'), function(){
-    expect(Triangle(3).to.equal("Side one equals", 3))
+describe('Triangle', function(){
+
+  it('Will check to see if Triangle via Triangle Inequality', function(){
+    expect(Triangle("3,0,0")).to.equal("These are not valid triangle dimensions")
   });
+
+  it('Will return a scalene triangle if sides are different values and meet triangle inequality', function(){
+    expect(Triangle("3,4,5")).to.equal("You have a Scalene Triangle");
+  });
+
+  it('Will return an isosceles triangle if only two sides are equal and meet triangle inequality', function(){
+    expect(Triangle("4,4,7")).to.equal("You have an Isosceles Triangle");
+  })
+
+  it('Will return an equilateral triangle if all sides are equal and meet the triangle inequality', function(){
+    expect(Triangle("5,5,5")).to.equal("You have an Equilateral Triangle")
+  });
+
+  it('Will accept float types for all conditions', function(){
+    expect(Triangle("0,1.5,0")).to.equal("These are not valid triangle dimensions")
+    expect(Triangle("3.25,4.4,5.5")).to.equal("You have a Scalene Triangle");
+    expect(Triangle("4.5,4.5,7")).to.equal("You have an Isosceles Triangle");
+    expect(Triangle("5.1,5.1,5.1")).to.equal("You have an Equilateral Triangle")
+  });
+
 });
-describe('wordOrder', function(){
-  it('will output the word from a one word string', function(){
-    expect(wordOrder("Hello")).to.equal("Hello");
-  });
-  
